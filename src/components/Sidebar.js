ec3d58127fbd158f4c037479c09c40ff9f9ff240
Irsyad <irsyadrbbn19@gmail.com>
2022-05-22 15:48:14 +0700

    Fixing sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Container, ListGroup, Nav, } from "react-bootstrap";
+import { Col, Container, Navbar, Nav, } from "react-bootstrap";
 import { FaHome, FaCalendarAlt, FaUsers } from "react-icons/fa";
 import  './Mystyle.css';
 
@@ -9,6 +9,17 @@ const Sidebar = () => {
       <Col>
     <Nav defaultActiveKey="/home" className="sidebar">
         <Container className='sidebarWrapper'>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link href="#action1">Home</Nav.Link>
+                    <Nav.Link href="#action2">Link</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
         <Nav.Link className='sidebarList'><FaHome className='sidebarIcon'/>Beranda</Nav.Link>
         <Nav.Link className='sidebarList' active><FaUsers className='sidebarIcon'/> Personnel List</Nav.Link>
         <Nav.Link className='sidebarList'><FaCalendarAlt className='sidebarIcon'/>Daily Attendance</Nav.Link>
